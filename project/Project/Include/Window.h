3e8bdef922309d986224762875f21b7dcde4ae2f
Caolan <barryc10@tcd.ie>
2018-11-19 18:41:45 +0000

    final project now in project folder

---
+++ project/Project/Include/Window.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "stdio.h"
+
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+class Window
+{
+public:
+	Window();
+
+	Window(GLint windowWidth, GLint windowHeight);
+
+	int Initialise();
+
+	GLint getBufferWidth() { return bufferWidth; }
+	GLint getBufferHeight() { return bufferHeight; }
+
+	bool getShouldClose() { return glfwWindowShouldClose(mainWindow); }
+
+	void swapBuffers() { glfwSwapBuffers(mainWindow); }
+
+	~Window();
+
+private:
+	GLFWwindow* mainWindow;
+
+	GLint width, height;
+	GLint bufferWidth, bufferHeight;
+
+	bool keys[1024];
+	//Store previous co-ordinates of the mouse
+	GLfloat lastx;
+	GLfloat lastY;
+
+	//Delta x, y for camera movement
+	GLfloat xCahnge;
+	GLfloat yChange;
+
+	//Dont immediately rotate at mouse position
+	bool firstMove;
+
+	void createCallbacks();	
+	static void handleInput(GLFWwindow* window, int key, int code, int action, int mode);
+	static void handleMouse(GLFWwindow* window, double x, double y);
+};
